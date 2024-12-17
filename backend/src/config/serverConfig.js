f985ee806b21d9bfdcb4e5e16227f392a83b46fd
JasrajChouhan <jasrajc945@gmail.com>
2024-12-18 02:08:27 +0530

    (feat): getEnv fcn for get env variable.

--- backend/src/config/serverConfig.js
+++ backend/src/config/serverConfig.js
@@ -2,6 +2,17 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-export const PORT = process.env.PORT || 3000;
+export const getEnv = (key , defaultValue = null) => {
+  try {
+    
+    const value = process.env[key];
+    if(value === undefined || null) { // not persent then thorw Error
+      throw new Error(`Environment variable "${key}" is not defined.`);
+    }
 
-export const REACT_PROJECT_COMMAND = process.env.REACT_PROJECT_COMMAND;
+    return value ; 
+  } catch (error) {
+    console.error(`Error retrieving environment variable: ${error.message}`);
+    return defaultValue;
+  }
+}
