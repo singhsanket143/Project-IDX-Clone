2713d03b21240c9ed0466889958a019ed3bb9381
JasrajChouhan <jasrajc945@gmail.com>
2024-12-19 05:09:58 +0530

    (feat): add tab state inside of use active file tab store

--- frontend/src/store/activeFileTabStore.js
+++ frontend/src/store/activeFileTabStore.js
@@ -1,16 +1,57 @@
-import { create } from 'zustand';
+import { create } from "zustand";
 
 export const useActiveFileTabStore = create((set) => {
     return {
         activeFileTab: null,
+        tabs: [],
         setActiveFileTab: (path, value, extension) => {
-            set( {
-                activeFileTab: {
-                    path: path,
-                    value: value,
-                    extension: extension
+            set((state) => {
+                const tabExists = state.tabs.some((tab) => tab.path === path);
+                return {
+                    activeFileTab: { path, value, extension },
+                    tabs: tabExists
+                        ? state.tabs
+                        : [
+                            ...state.tabs,
+                            { path, name: path.replace(/\\/g, '/').split('/').pop(), extension, value }
+                        ]
+                };
+            });
+        },
+
+        setActiveTab: (path) => {
+            set((state) => {
+                const tab = state.tabs.find((t) => t.path === path);
+                console.log(state.tabs);
+                
+                if (tab) {
+                    return {
+                        activeFileTab: { ...tab }
+                    };
                 }
-            })
+                return {
+                    activeFileTab: null
+                };
+            });
+        },
+
+        closeTab: (path) => {
+            set((state) => {
+                const updatedTabs = state.tabs.filter((tab) => tab.path !== path);
+                return {
+                    tabs: updatedTabs,
+                    activeFileTab: updatedTabs.length > 0
+                        ? updatedTabs[updatedTabs.length - 1]
+                        : null
+                };
+            });
+        },
+
+        clearTabs: () => {
+            set({
+                tabs: [],
+                activeFileTab: null
+            });
         }
-    }
+    };
 });
